e56f80476c60689224413715365764785d6a5c10
Isaac Sanchez <jelito@gmail.com>
2018-05-05 15:21:44 +0100

    Update

--- Dental-Clinic/src/controller/dao/ClinicDBController.java
+++ Dental-Clinic/src/controller/dao/ClinicDBController.java
@@ -1,7 +1,11 @@
 package controller.dao;
 
+import java.sql.SQLException;
+
 import controller.ClinicController;
 import model.Clinic;
+import model.Patient;
+import model.ProcedureType;
 
 public class ClinicDBController extends AbstractClinicStorageController {
 
@@ -24,10 +28,24 @@ public class ClinicDBController extends AbstractClinicStorageController {
 	
 	private Clinic getClinicFromDB() {
 		Clinic result = null;
+		Patient pat = null;
+		ProcedureType pt = null;
 		
 		db.getDBConnection();
-		db.QueryDB("SELECT * FROM Clinic");
+		db.QueryDB("SELECT * FROM Patients");
 		result = (Clinic) db.rs;
+		
+		try {
+			while(db.rs.next()){
+				//create your object from teh resultset here
+//				pat = db.rs.n
+			}
+		} catch (SQLException e) {
+			System.out.println("Error reading patient");
+			e.printStackTrace();
+		}
+		
+		
 		db.CloseDB();
 		return result;
 	}
